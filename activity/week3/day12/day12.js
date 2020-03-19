ca93779cc91d29d4a662f8ae7c783a9acdebd96a
bharathreddyza <bharathreddyza@gmail.com>
2020-03-19 18:59:10 +0530

    day3

---
+++ activity/week3/day12/day12.js
@@ -0,0 +1,52 @@
+/* var indices = [];
+var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+var element = 'a';
+var idx = array.indexOf(element);
+while (idx != -1) {
+  indices.push(idx);
+  idx = array.indexOf(element, idx + 1);
+}
+console.log(indices); */
+//////  make it into for loop 
+var indices = [];
+var array = ['a','b','c','d'];
+var element = 'a';
+var idx = array.indexOf(element);
+for(i=0;)
+
+
+// Ffunctions get a new memory every run or invocation 
+ function multilyby2(input){
+     const result = input * 2 ;
+     return result;
+ }
+ const output = multilyby2(6)
+
+function creatdfunction(){
+    function multiplyby2(num){
+        return num *2 
+    }
+    return multilyby2
+    }
+}
+const generatedfunv = createfunction()
+const result = generated(4)
+//funtion with memorys 
+// 3 
+//9
+// 3
+//4
+//5
+//7 and kill the function createFunction but multiplyby2 function is returned to generatedfunc
+// 9
+// 10 
+//9
+//excute 4 in multiplyby2 
+//
+//
+//
+//
+//
+//
+//
+//
