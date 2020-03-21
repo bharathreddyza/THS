ec4b0e3da53c776cacae80382103b1105871efbe
bharathreddyza <bharathreddyza@gmail.com>
2020-03-21 20:39:49 +0530

    algos/

---
+++ algos/algos.js
@@ -0,0 +1,23 @@
+//  1)Write a program to given input Check whether  Even or odd.
+var evenOdd = [12, 43, 4, 45, 45];
+for(var i=0; i<evenAndOdd.legnth; i++){
+    var a;
+    switch(evenAndOdd){
+        case evenAndOdd(i) % 2 === 0:
+       console.log(evenAndOdd + " is even");
+        case  evenAndOdd(i) % 2 !== 0:
+          console.log(evenAndOdd + " is odd")   
+    }
+    
+}
+
+//16)Write a program  to find Mean of given Array Elements?
+var arr = [21,4,554,33,44];
+var a = 0;
+var b = 0;
+for(var i =0;i<arr.length;i++){
+   a += arr[i]
+ b = a / arr.length
+} 
+
+
